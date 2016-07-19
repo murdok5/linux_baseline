50dfae68a843747118c3d0177bf60bab88409b6d
murdok5 <mike.smith@puppet.com>
2016-07-19 09:36:22 -0700

    init baseline

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class linux_baseline {
+  
+  include ntp
+  include epel
+  
+  # packages
+  
+  package { 'openssh':
+    ensure => present,
+  }
+
+  package { 'vim':
+    ensure => latest,
+  }
+  
+  package { 'openissl':
+    ensure => '1.0.1f-1ubuntu2.18',
+  }
+
+  # services
+  
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+
+  # users
+
+  accounts::user { 'athena':
+    uid      => 4001,
+    gid      => 4001,
+    shell    => '/bin/bash',
+    password => 'password123',
+    sshkeys  => "ssh-rsa AAAA...",
+    locked   => false,
+  }
+
+
+}
